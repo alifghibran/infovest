25635698c84aded1599cf0b21a13ad02c5ba60a7
alifghibran <alifarda2719@gmail.com>
2018-02-01 14:51:30 +0700

    versi 1.0.0

---
+++ class/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header</title>
+	<style type="text/css">
+		body{
+			margin: 0;
+			padding: 0;
+			font-family: "Raleway","Sugeo UI",sans-serif,arial;
+		}
+		.center{
+			width: 1135px;
+			padding: 0;
+			margin: 0 auto;
+		}
+		.clear{
+			float: none;
+			clear: both;
+		}
+		#header{
+			padding: 5px 0;
+			margin: 0;
+			background-color: #3aa3e6;
+		}
+		.logo{
+			float: left;
+			padding: 0;
+			margin: 0;
+		}
+		.menu{
+			float: right;
+			padding: 0;
+			margin: 0;
+		}
+		.menu ul{
+			padding: 0;
+			margin: 0;
+		}
+		.menu ul li{
+			display: inline-block;
+			list-style-type: none;
+			padding: 0;
+			margin: 0 5px;
+		}
+		.menu ul li a{
+			text-decoration: none;
+			color: #fff;
+			font-size: 14pt;
+		}
+	</style>
+</head>
+<body>
+
+</body>
+</html>
---
+++ tentang.php
@@ -0,0 +1,16 @@
+<?php
+	require_once 'class/header.php';
+	if ($_SESSION['level'] == 1) {
+  	header("Location: index.php");
+}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tentang</title>
+	<link rel="stylesheet" type="text/css" href="css/tentang.css">
+</head>
+<body>
+asdasdasd
+</body>
+</html>
